27c08b928042c7f66e0ba0e7d554c729e58b7a85
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2022-09-27 15:43:27 +0300

    v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,64 @@
 "use strict";
-import { tasksData } from "./data";
+// import { tasksData } from "./data";
 import { makeTable } from "./tasks";
 import { countsTask } from "./counts";
 
-let tasks = [...tasksData];
+let tasks = [
+  {
+    id: 1,
+    name: "Shoping list",
+    created: "4/21/2021",
+    category: "Task",
+    content: "Tomatoes, bread",
+    dates: "",
+    type: "active",
+  },
+  {
+    id: 2,
+    name: "The teory of evolution",
+    created: "4/27/2021",
+    category: "Random Thought",
+    content: "The evolution",
+    dates: "",
+    type: "active",
+  },
+  {
+    id: 3,
+    name: "New Feature",
+    created: "3/5/2021",
+    category: "Idea",
+    content: "Implement new feater",
+    dates: "5/5/2021",
+    type: "active",
+  },
+  {
+    id: 4,
+    name: "Shoping list",
+    created: "5/6/2021",
+    category: "Task",
+    content: "Bread, milk, tomato",
+    dates: "",
+    type: "active",
+  },
+  {
+    id: 5,
+    name: "About React",
+    created: "5/6/2021",
+    category: "Random Thought",
+    content: "React evolution",
+    dates: "",
+    type: "active",
+  },
+  {
+    id: 6,
+    name: "New Feature",
+    created: "5/7/2021",
+    category: "Idea",
+    content: "Implement new script",
+    dates: "5/8/2021",
+    type: "active",
+  },
+];
 let archivedTasks = [];
 let status = "";
 
